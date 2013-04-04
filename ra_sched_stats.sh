5d067402827e4160b25fb41711d7a50f2a606535
sheykes <stefan.heykes@ewetel.net>
2013-04-05 01:38:13 +0200

    added comments

--- ra_sched_stats.sh
+++ ra_sched_stats.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Copyright (C) Stefan Heykes 2013
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
 # Generating statistics for the monthly RadioAstron block schedule files published by ASC
 # Optimized for the format used since Feb 2013 - older files need to be reformatted
 # Block schedules can be downloaded here: ftp://jet.asc.rssi.ru/outgoing/yyk/Radioastron/block_schedule/
 # $1 = Block schedule file $2 = Output file, also needed as a temporary buffer!
 # This script generates to output files - "$2" contains the output as BBCode, "$2.csv" as Comma-Separated-Values
-# This script uses BSD date to convert date/time
+# This script uses BSD date to convert date/time!
+#
+# If you fix any bugs in this script (or manage to use GNU date, which seems to be better for Linux-Users), please let me know ;-)
 
 # Extract time, baselines and used bands from the file - analyse only those observations for which the baseline is given
 grep -v '#' "$1" | grep '\(Band:\)\|\( *xED\)\|\(Start(UT)\)\|\(Stop(UT)\)' | grep -B 3 'Comment' | grep '\(Band:\)\|\( *xED\)\|\(Start(UT)\)\|\(Stop(UT)\)' > "$2"
@@ -47,134 +56,133 @@ SUM_COUNT=`expr $SUM_COUNT / 4`
 # Analyzing single observations
 I=0
 while [ $I -lt $SUM_COUNT ];do
-	I=`expr $I + 1`
-	POS=`expr $I \* 4 - 3`
-	TIME_START=`head -n "$POS" "$2" | tail -n 1 | sed -e '/S/s/Start(UT): //g'`
+I=`expr $I + 1`
+POS=`expr $I \* 4 - 3`
+TIME_START=`head -n "$POS" "$2" | tail -n 1 | sed -e '/S/s/Start(UT): //g'`
 # Echo start time as debugging information - errors can occur when the input file entry differs from the standard format
 # in case of errors simply adjust the input file before running this script again
-	echo "$TIME_START"
-	TIME_START=`date -j -f "%d.%m.%Y %H:%M:%S" "$TIME_START" "+%s"`
-	POS=`expr $POS + 1`
-	TIME_STOP=`head -n "$POS" "$2" | tail -n 1 | sed -e '/S/s/Stop(UT) : //g'`
-	TIME_STOP=`date -j -f "%d.%m.%Y %H:%M:%S" "$TIME_STOP" "+%s"`
-	TIME_OBSERVATION=`expr $TIME_STOP - $TIME_START`
-	SUM_TIME=`expr $SUM_TIME + $TIME_OBSERVATION`
-	POS=`expr $POS + 1`
-	#head -n "$POS" "$2" | tail -n 1
-	BAND=`head -n "$POS" "$2" | tail -n 1 | sed -e '/B/s/Band: //g'`
-	POS=`expr $POS + 1`
-	#head -n "$POS" "$2" | tail -n 1
-	BASELINE=`head -n "$POS" "$2" | tail -n 1 | sed -e '/C/s/Comments: //g' | sed 's/\([0-9]*\).*/\1/'`
+echo "$TIME_START"
+TIME_START=`date -j -f "%d.%m.%Y %H:%M:%S" "$TIME_START" "+%s"`
+POS=`expr $POS + 1`
+TIME_STOP=`head -n "$POS" "$2" | tail -n 1 | sed -e '/S/s/Stop(UT) : //g'`
+TIME_STOP=`date -j -f "%d.%m.%Y %H:%M:%S" "$TIME_STOP" "+%s"`
+TIME_OBSERVATION=`expr $TIME_STOP - $TIME_START`
+SUM_TIME=`expr $SUM_TIME + $TIME_OBSERVATION`
+POS=`expr $POS + 1`
+#head -n "$POS" "$2" | tail -n 1
+BAND=`head -n "$POS" "$2" | tail -n 1 | sed -e '/B/s/Band: //g'`
+POS=`expr $POS + 1`
+#head -n "$POS" "$2" | tail -n 1
+BASELINE=`head -n "$POS" "$2" | tail -n 1 | sed -e '/C/s/Comments: //g' | sed 's/\([0-9]*\).*/\1/'`
 
-	case "${BAND:0:1}" in
-		K)
-			K_AVRG=`expr $K_AVRG + $BASELINE`
-			K_TIME=`expr $K_TIME + $TIME_OBSERVATION`
-			if [ $K_MIN -gt $BASELINE ]
-			then
-				K_MIN=$BASELINE
-			fi
-			if [ $K_MAX -lt $BASELINE ]
-			then
-				K_MAX=$BASELINE
-			fi
-			;;
-		C)
-			C_AVRG=`expr $C_AVRG + $BASELINE`
-			C_TIME=`expr $C_TIME + $TIME_OBSERVATION`
-			if [ $C_MIN -gt $BASELINE ]
-			then
-				C_MIN=$BASELINE
-			fi
-			if [ $C_MAX -lt $BASELINE ]
-			then
-				C_MAX=$BASELINE
-			fi
-			;;
-		L)
-			L_AVRG=`expr $L_AVRG + $BASELINE`
-			L_TIME=`expr $L_TIME + $TIME_OBSERVATION`
-			if [ $L_MIN -gt $BASELINE ]
-			then
-				L_MIN=$BASELINE
-			fi
-			if [ $L_MAX -lt $BASELINE ]
-			then
-				L_MAX=$BASELINE
-			fi
-			;;
-		P)
-			P_AVRG=`expr $P_AVRG + $BASELINE`
-			P_TIME=`expr $P_TIME + $TIME_OBSERVATION`
-			if [ $P_MIN -gt $BASELINE ]
-			then
-				P_MIN=$BASELINE
-			fi
-			if [ $P_MAX -lt $BASELINE ]
-			then
-				P_MAX=$BASELINE
-			fi
-			;;
-		*)
-			;;
-	esac 
-	
-	if  [ ${BAND:0:1} != ${BAND:1:1} ]
-	then
-		case "${BAND:1:1}" in
-			K)
-				K_AVRG=`expr $K_AVRG + $BASELINE`
-				K_TIME=`expr $K_TIME + $TIME_OBSERVATION`
-				if [ $K_MIN -gt $BASELINE ]
-				then
-					K_MIN=$BASELINE
-				fi
-				if [ $K_MAX -lt $BASELINE ]
-				then
-					K_MAX=$BASELINE
-				fi
-				;;
-			C)
-				C_AVRG=`expr $C_AVRG + $BASELINE`
-				C_TIME=`expr $C_TIME + $TIME_OBSERVATION`
-				if [ $C_MIN -gt $BASELINE ]
-				then
-					C_MIN=$BASELINE
-				fi
-				if [ $C_MAX -lt $BASELINE ]
-				then
-					C_MAX=$BASELINE
-				fi
-				;;
-			L)
-				L_AVRG=`expr $L_AVRG + $BASELINE`
-				L_TIME=`expr $L_TIME + $TIME_OBSERVATION`
-				if [ $L_MIN -gt $BASELINE ]
-				then
-					L_MIN=$BASELINE
-				fi
-				if [ $L_MAX -lt $BASELINE ]
-				then
-					L_MAX=$BASELINE
-				fi
-				;;
-			P)
-				P_AVRG=`expr $P_AVRG + $BASELINE`
-				P_TIME=`expr $P_TIME + $TIME_OBSERVATION`
-				if [ $P_MIN -gt $BASELINE ]
-				then
-					P_MIN=$BASELINE
-				fi
-				if [ $P_MAX -lt $BASELINE ]
-				then
-					P_MAX=$BASELINE
-				fi
-				;;
-			*)
-				;;
-		esac 
-	fi
-done 
+case "${BAND:0:1}" in
+K)
+K_AVRG=`expr $K_AVRG + $BASELINE`
+K_TIME=`expr $K_TIME + $TIME_OBSERVATION`
+if [ $K_MIN -gt $BASELINE ]
+then
+K_MIN=$BASELINE
+fi
+if [ $K_MAX -lt $BASELINE ]
+then
+K_MAX=$BASELINE
+fi
+;;
+C)
+C_AVRG=`expr $C_AVRG + $BASELINE`
+C_TIME=`expr $C_TIME + $TIME_OBSERVATION`
+if [ $C_MIN -gt $BASELINE ]
+then
+C_MIN=$BASELINE
+fi
+if [ $C_MAX -lt $BASELINE ]
+then
+C_MAX=$BASELINE
+fi
+;;
+L)
+L_AVRG=`expr $L_AVRG + $BASELINE`
+L_TIME=`expr $L_TIME + $TIME_OBSERVATION`
+if [ $L_MIN -gt $BASELINE ]
+then
+L_MIN=$BASELINE
+fi
+if [ $L_MAX -lt $BASELINE ]
+then
+L_MAX=$BASELINE
+fi
+;;
+P)
+P_AVRG=`expr $P_AVRG + $BASELINE`
+P_TIME=`expr $P_TIME + $TIME_OBSERVATION`
+if [ $P_MIN -gt $BASELINE ]
+then
+P_MIN=$BASELINE
+fi
+if [ $P_MAX -lt $BASELINE ]
+then
+P_MAX=$BASELINE
+fi
+;;
+*)
+;;
+esac
+if [ ${BAND:0:1} != ${BAND:1:1} ]
+then
+case "${BAND:1:1}" in
+K)
+K_AVRG=`expr $K_AVRG + $BASELINE`
+K_TIME=`expr $K_TIME + $TIME_OBSERVATION`
+if [ $K_MIN -gt $BASELINE ]
+then
+K_MIN=$BASELINE
+fi
+if [ $K_MAX -lt $BASELINE ]
+then
+K_MAX=$BASELINE
+fi
+;;
+C)
+C_AVRG=`expr $C_AVRG + $BASELINE`
+C_TIME=`expr $C_TIME + $TIME_OBSERVATION`
+if [ $C_MIN -gt $BASELINE ]
+then
+C_MIN=$BASELINE
+fi
+if [ $C_MAX -lt $BASELINE ]
+then
+C_MAX=$BASELINE
+fi
+;;
+L)
+L_AVRG=`expr $L_AVRG + $BASELINE`
+L_TIME=`expr $L_TIME + $TIME_OBSERVATION`
+if [ $L_MIN -gt $BASELINE ]
+then
+L_MIN=$BASELINE
+fi
+if [ $L_MAX -lt $BASELINE ]
+then
+L_MAX=$BASELINE
+fi
+;;
+P)
+P_AVRG=`expr $P_AVRG + $BASELINE`
+P_TIME=`expr $P_TIME + $TIME_OBSERVATION`
+if [ $P_MIN -gt $BASELINE ]
+then
+P_MIN=$BASELINE
+fi
+if [ $P_MAX -lt $BASELINE ]
+then
+P_MAX=$BASELINE
+fi
+;;
+*)
+;;
+esac
+fi
+done
 
 # Calculating average baselines
 
@@ -187,7 +195,7 @@ P_AVRG=`expr $P_AVRG / $P_COUNT`
 #Generate CSV
 echo "Total number of observations;$SUM_COUNT" > "$2.csv"
 echo "Total observation time [s];$SUM_TIME" >> "$2.csv"
-echo "Band;Number;Min.Baseline;Max.Baseline;Avrg.Baseline;Time"  >> "$2.csv"
+echo "Band;Number;Min.Baseline;Max.Baseline;Avrg.Baseline;Time" >> "$2.csv"
 echo "K(1.35cm);$K_COUNT;$K_MIN;$K_MAX;$K_AVRG;$K_TIME" >> "$2.csv"
 echo "C(6,2cm);$C_COUNT;$C_MIN;$C_MAX;$C_AVRG;$C_TIME" >> "$2.csv"
 echo "L(18cm);$L_COUNT;$L_MIN;$L_MAX;$L_AVRG;$L_TIME" >> "$2.csv"
@@ -213,11 +221,11 @@ P_COUNT=`grep -v '#' "$1" | grep 'Band:' | grep 'P' | wc -l`
 
 echo "Total number of observations: $SUM_COUNT"
 echo "Total observation time: $SUM_TIME"
-echo "Band      Number  Min.BL  Max.BL  Avrg.BL   Time"
-echo "K(1.35cm) $K_COUNT      $K_MIN       $K_MAX      $K_AVRG     $K_TIME"
-echo "C(6,2cm)  $C_COUNT      $C_MIN       $C_MAX      $C_AVRG     $C_TIME"
-echo "L(18cm)   $L_COUNT      $L_MIN       $L_MAX      $L_AVRG     $L_TIME"
-echo "P(92cm)   $P_COUNT      $P_MIN       $P_MAX      $P_AVRG     $P_TIME"
+echo "Band Number Min.BL Max.BL Avrg.BL Time"
+echo "K(1.35cm) $K_COUNT $K_MIN $K_MAX $K_AVRG $K_TIME"
+echo "C(6,2cm) $C_COUNT $C_MIN $C_MAX $C_AVRG $C_TIME"
+echo "L(18cm) $L_COUNT $L_MIN $L_MAX $L_AVRG $L_TIME"
+echo "P(92cm) $P_COUNT $P_MIN $P_MAX $P_AVRG $P_TIME"
 
 echo ""
 echo ""
